cdb939e5a6f18c26abe75498abee59295fc600da
raj rana <raj.rana507@gmail.com>
2021-10-31 17:13:35 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import Quiz from "@/components/Quiz.vue"
+</script>
+
+<template>
+  <div class="container">
+    <Quiz />
+  </div>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Merriweather:ital@0;1&family=Mukta&family=Roboto+Slab&display=swap');
+
+
+*{
+  box-sizing: border-box;
+}
+body { 
+  margin:0;
+  font-family: 'Merriweather', serif;
+  font-family: 'Mukta', sans-serif;
+  font-family: 'Roboto Slab', serif;
+}
+.container { 
+  background: #8e9eab;  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #eef2f3, #8e9eab);  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #eef2f3, #8e9eab); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+  height: 100vh;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ src/components/Quiz.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="wrapper" >
+        <div class="question">
+            <p> This is Questions </p>
+            <form action="">
+                <div class="option">
+                    <input type="radio" id="option1" name="option" value="a">
+                    <label for="option1"> option 1</label>
+                </div>
+                <div class="option" >
+                    <input type="radio" id="option2" name="option" value="b">
+                    <label for="option2"> option 2</label>
+                </div>
+                <div class="option">
+                    <input type="radio" id="option3" name="option" value="c">
+                    <label for="option3"> option 3</label>
+                </div>
+                <div class="option">
+                    <input type="radio" id="option4" name="option" value="d">
+                    <label for="option4"> option 4</label>
+                </div>
+            </form>
+        </div>
+        <div class="btn"> this submit button</div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+
+</script>
+
+<style scoped>
+.wrapper {
+    height: 300px;
+    width: 400px;
+    background-color: #fff;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+}
+
+.question { 
+    flex: 1;
+    text-align: center;
+}
+
+.option {
+    padding: 0.5rem 0.25rem;
+}
+.btn { 
+    background-color: #4273F4;
+    padding: 1rem 0.5rem;
+    border-bottom-left-radius: 10px;
+    border-bottom-right-radius: 10px;
+    text-align: center;
+    color: #fff;
+}
+
+</style>
